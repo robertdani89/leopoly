05667877a1b19561ed74d2e0c9ef5ec1b17ba225
Robert Dani <robert.dani@cae.com>
2024-04-03 13:38:29 +0200

    feat: nx setup

---
+++ libs/frontend/feature-landing/src/lib/feature-landing/feature-landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-feature-landing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './feature-landing.component.html',
+  styleUrl: './feature-landing.component.css',
+})
+export class FeatureLandingComponent {}
---
+++ libs/frontend/feature-landing/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { FeatureLandingComponent } from './feature-landing/feature-landing.component';
+
+export const featureLandingRoutes: Route[] = [
+  { path: '', component: FeatureLandingComponent },
+];
